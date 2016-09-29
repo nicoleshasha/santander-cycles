a79d4565df7c15c7cf1a6c079fbaf144ed229820
Nicole <nicoleshasha@hotmail.co.uk>
2016-09-29 11:42:18 +0100

    Added return docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+      @bike = bike
+  end
+
+attr_reader :bike
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock_bike).with(1).argument}
 
   it 'releases working bikes' do
     bike = subject.release_bike
@@ -9,7 +10,14 @@ describe DockingStation do
   end
 
   it 'docks a bike' do
-    bike = subject.dock_bike
-    expect(bike).to be_docked
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
   end
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
